fbb77ee246d64a277dec099a938513d59a8216aa
AntoniobB09 <antblanco@gmail.com>
2025-01-23 12:29:20 -0400

    FEAT: Tests de recuperar y update

--- authTests/AuthControllerTests.cs
+++ authTests/AuthControllerTests.cs
@@ -17,6 +17,15 @@ using UsersMicroservice.src.user.application.repositories;
 using UsersMicroservice.src.user.domain.value_objects;
 using UsersMicroservice.src.user.domain;
 using UsersMicroservice.src.auth.application.commands.login;
+using Microsoft.Extensions.Configuration;
+using Sprache;
+using UsersMicroservice.src.auth.application.commands.update_credentials.types;
+using UsersMicroservice.src.auth.application.commands.update_credentials;
+using UsersMicroservice.src.auth.application.commands.recover_password.types;
+using UsersMicroservice.src.auth.application.commands.recover_password;
+using System.Net;
+using System.Xml.Linq;
+using UsersMicroservice.core.Infrastructure;
 
 namespace TestUserMicroservice.authTests
 {
@@ -26,6 +35,7 @@ namespace TestUserMicroservice.authTests
         private readonly Mock<ITokenAuthenticationService> _mockTokenAuthenticationService;
         private readonly Mock<ICredentialsRepository> _mockCredentialsRepository;
         private readonly Mock<IUserRepository> _mockUserRepository;
+        private readonly Mock<IConfiguration> _mockConfiguration;
         private readonly AuthController _controller;
 
         public AuthControllerTests()
@@ -34,11 +44,14 @@ namespace TestUserMicroservice.authTests
             _mockTokenAuthenticationService = new Mock<ITokenAuthenticationService>();
             _mockCredentialsRepository = new Mock<ICredentialsRepository>();
             _mockUserRepository = new Mock<IUserRepository>();
+            _mockConfiguration = new Mock<IConfiguration>();
+
             _controller = new AuthController(
                 _mockCredentialsRepository.Object,
                 _mockUserRepository.Object,
                 _mockCryptoService.Object,
-                _mockTokenAuthenticationService.Object
+                _mockTokenAuthenticationService.Object,
+                _mockConfiguration.Object
             );
         }
 
@@ -54,17 +67,18 @@ namespace TestUserMicroservice.authTests
             var result = Result<LoginUserResponse>.Success(response);
 
             _mockCryptoService.Setup(service => service.Compare(It.IsAny<string>(), It.IsAny<string>())).ReturnsAsync(true);
-            _mockCredentialsRepository.Setup(repo => repo.GetCredentialsByEmail(It.IsAny<string>())).ReturnsAsync(_Optional<Credentials>.Of(new Credentials("9fb9be1e-37a6-457b-8719-6a832185b5d3", userId, "test@gamil.com", "$2a$11$HQ4egknPnudciE3EznetcerpuMxy / zcqmgFiYeFgSs2JTLJQRMuZq")));
+            _mockCredentialsRepository.Setup(repo => repo.GetCredentialsByEmail(It.IsAny<string>()))
+                .ReturnsAsync(_Optional<Credentials>.Of(new Credentials("9fb9be1e-37a6-457b-8719-6a832185b5d3", userId, "test@gamil.com", "$2a$11$HQ4egknPnudciE3EznetcerpuMxy / zcqmgFiYeFgSs2JTLJQRMuZq")));
             _mockUserRepository.Setup(repo => repo.GetUserById(It.IsAny<UserId>())).ReturnsAsync(_Optional<User>.Of(user));
             _mockTokenAuthenticationService.Setup(service => service.Authenticate(It.IsAny<string>(), It.IsAny<string>())).Returns(new TokenResponse(accessToken, 3600));
-            
+
             // Act
-          
+
             var resultAction = await _controller.Login(command);
 
             // Assert
             Assert.True(resultAction is OkObjectResult);
-            
+
 
 
         }
@@ -76,8 +90,10 @@ namespace TestUserMicroservice.authTests
             var command = new LoginUserCommand("test@gmail.com", "wrongpassword");
             var result = Result<LoginUserResponse>.Failure(new Exception("Invalid credentials"));
 
-            _mockCryptoService.Setup(service => service.Compare(It.IsAny<string>(), It.IsAny<string>())).ReturnsAsync(false);
-            _mockCredentialsRepository.Setup(repo => repo.GetCredentialsByEmail(It.IsAny<string>())).ReturnsAsync(_Optional<Credentials>.Of(new Credentials("1", "1", "test@example.com", "hashedPassword")));
+            _mockCryptoService.Setup(service => service.Compare(It.IsAny<string>(), It.IsAny<string>()))
+                .ReturnsAsync(false);
+            _mockCredentialsRepository.Setup(repo => repo.GetCredentialsByEmail(It.IsAny<string>()))
+                .ReturnsAsync(_Optional<Credentials>.Of(new Credentials("1", "1", "test@example.com", "hashedPassword")));
 
             // Act
             var resultAction = await _controller.Login(command);
@@ -93,7 +109,8 @@ namespace TestUserMicroservice.authTests
             // Arrange
             var command = new LoginUserCommand("test@example.com", "password");
 
-            _mockCredentialsRepository.Setup(repo => repo.GetCredentialsByEmail(It.IsAny<string>())).ReturnsAsync(_Optional<Credentials>.Empty());
+            _mockCredentialsRepository.Setup(repo => repo.GetCredentialsByEmail(It.IsAny<string>())).
+                ReturnsAsync(_Optional<Credentials>.Empty());
 
             // Act
             var resultAction = await _controller.Login(command);
@@ -102,6 +119,114 @@ namespace TestUserMicroservice.authTests
             Assert.True(resultAction is UnauthorizedObjectResult);
         }
 
+        [Fact(DisplayName = "Test When UpdateCredentials Method Resturns OkObjectResult ")]
 
+        public async Task UpdateCredentials_ValidCommand_ReturnsOkResult()
+        {
+            // Arrange
+            var userId = "2e512140-dfd7-4927-ba48-8986d243f638";
+            var user = User.Create(new UserId(userId), new UserName("Test User"), new UserPhone("+584242374999"), new UserRole("admin"), new DepartmentId("5fb9be1e-37a6-457b-8719-6a832185b5d3"));
+            var command = new UpdateCredentialsCommand(userId, "test@gmail.com", "newpassword");
+
+            _mockCredentialsRepository.Setup(repo => repo.GetCredentialsByUserId(It.IsAny<string>()))
+                .ReturnsAsync(_Optional<Credentials>.Of(new Credentials("9fb9be1e-37a6-457b-8719-6a832185b5d3", userId, "test@gamil.com", "$2a$11$HQ4egknPnudciE3EznetcerpuMxy / zcqmgFiYeFgSs2JTLJQRMuZq")));
+            _mockCredentialsRepository.Setup(repo => repo.UpdateCredentials(It.IsAny<string>(), It.IsAny<string>(), It.IsAny<string>()));
+            _mockCryptoService.Setup(service => service.Compare(It.IsAny<string>(), It.IsAny<string>()))
+                                      .ReturnsAsync(true);
+
+            var service = new UpdateCredentialsCommandHandler(_mockCredentialsRepository.Object, _mockCryptoService.Object);
+            _mockCryptoService.Setup(cs => cs.Hash(It.IsAny<string>())).ReturnsAsync("hashedpassword");
+
+            // Act
+            var result = await _controller.UpdateCredentials(command);
+
+            // Assert
+            Assert.True(result is OkObjectResult);
+
+        }
+
+        [Fact(DisplayName = "Test When UpdateCredentials Method Resturns BadRequestObjectResult ")]
+
+        public async Task UpdateCredentials_InvalidCommand_ReturnsBadRequest()
+        {
+            // Arrange
+            var userId = "2e512140-dfd7-4927-ba48-8986d243f638";
+            var user = User.Create(new UserId(userId), new UserName("Test User"), new UserPhone("+584242374999"), new UserRole("admin"), new DepartmentId("5fb9be1e-37a6-457b-8719-6a832185b5d3"));
+            var command = new UpdateCredentialsCommand(userId, "newetest.com", "password");
+
+            _mockCredentialsRepository.Setup(repo => repo.GetCredentialsByUserId(It.IsAny<string>()))
+                .ReturnsAsync(_Optional<Credentials>.Of(new Credentials("9fb9be1e-37a6-457b-8719-6a832185b5d3", userId, "test@gamil.com", "$2a$11$HQ4egknPnudciE3EznetcerpuMxy / zcqmgFiYeFgSs2JTLJQRMuZq")));
+            _mockCredentialsRepository.Setup(repo => repo.UpdateCredentials(It.IsAny<string>(), It.IsAny<string>(), It.IsAny<string>()));
+            _mockCryptoService.Setup(service => service.Compare(It.IsAny<string>(), It.IsAny<string>()))
+                 .ReturnsAsync(true);
+
+            var service = new UpdateCredentialsCommandHandler(_mockCredentialsRepository.Object, _mockCryptoService.Object);
+            _mockCryptoService.Setup(cs => cs.Hash(It.IsAny<string>())).ReturnsAsync("hashedpassword");
+
+            // Act
+            var result = await _controller.UpdateCredentials(command);
+
+            // Assert
+            Assert.True(result is BadRequestObjectResult);
+        }
+
+        [Fact]
+        public async Task RecoverPassword_ReturnsOkResult_ValidCommand()
+        {
+            // Arrange
+            var userId = "2e512140-dfd7-4927-ba48-8986d243f638";
+            var command = new RecoverPasswordCommand("test@gmail.com");
+            var subject = "Test Subject";
+            var body = "Test Body";
+            _mockCryptoService.Setup(service => service.Compare(It.IsAny<string>(), It.IsAny<string>()))
+                .ReturnsAsync(true);
+            _mockCredentialsRepository.Setup(repo => repo.GetCredentialsByEmail(It.IsAny<string>()))
+                .ReturnsAsync(_Optional<Credentials>.Of(new Credentials("9fb9be1e-37a6-457b-8719-6a832185b5d3", userId, "test@gamil.com", "$2a$11$HQ4egknPnudciE3EznetcerpuMxy / zcqmgFiYeFgSs2JTLJQRMuZq")));
+     
+            _mockCredentialsRepository.Setup(repo => repo.UpdateCredentials(It.IsAny<string>(), It.IsAny<string>(), It.IsAny<string>()));
+            _mockConfiguration.Setup(config => config["SmtpSettings:SmtpHost"]).Returns("smtp.test.com");
+            _mockConfiguration.Setup(config => config["SmtpSettings:SmtpPort"]).Returns("587");
+            _mockConfiguration.Setup(config => config["EmailSettings:From"]).Returns("noreply@example.com");
+           
+           
+            var emailContent = new EmailContent(subject, body);
+            var service = new RecoverPasswordCommandHandler(_mockCredentialsRepository.Object, _mockCryptoService.Object);
+
+
+            // Act
+            var result = await _controller.RecoverPassword(command);
+            var response = await service.Execute(command);
+
+
+            // Assert
+
+            Assert.True(response.IsSuccessful );
+
+        }
+
+       
+
+        [Fact]
+        public async Task RecoverPassword_ReturnsBadRequest_InvalidCommand()
+        {
+            // Arrange
+
+            var command = new RecoverPasswordCommand("invalidgmail.com");
+
+
+            _mockCredentialsRepository.Setup(repo => repo.GetCredentialsByEmail(It.IsAny<string>())).
+               ReturnsAsync(_Optional<Credentials>.Empty());
+
+            var service = new RecoverPasswordCommandHandler(_mockCredentialsRepository.Object, _mockCryptoService.Object);
+
+
+            // Act
+            var result = await _controller.RecoverPassword(command);
+
+            // Assert
+            Assert.True(result is BadRequestObjectResult);
+
+
+        }
     }
 }
--- obj/Debug/net8.0/TestUserMicroservice.AssemblyInfo.cs
+++ obj/Debug/net8.0/TestUserMicroservice.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestUserMicroservice")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+73fef45b4ad084620c4a1b90b49c7d5525cc59cb")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestUserMicroservice")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestUserMicroservice")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
